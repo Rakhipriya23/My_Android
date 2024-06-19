72e54ec70cf30d42f86d92078faa214acc2d9378
Rakhipriya23 <rakhipriyadarshini08@gmail.com>
2024-06-19 20:59:37 +0530

    implented Chips

--- app/src/main/java/com/example/myandroid/MainActivity.kt
+++ app/src/main/java/com/example/myandroid/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,8 +19,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.AlertDialogDefaults
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgeDefaults.containerColor
 import androidx.compose.material3.BadgedBox
@@ -30,10 +34,15 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -57,7 +66,14 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BadgeM3()
+                    Column (
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement =  Arrangement.Center
+
+                    ){
+                       AssistChipM3()
+                    }
                 }
             }
 
@@ -67,43 +83,17 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-    //badges
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun BadgeM3() {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            BadgedBox(
-                badge = {
-                    Badge {
-                        Text(text = "99+")
-                    }
-                }
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Email, contentDescription = "Email",
-                    modifier = Modifier
-                        .size(50.dp)
-
-
-                )
-            }
+@Composable
+fun AssistChipM3(){
+    AssistChip(
+        onClick = { /* do something*/ }, label = { Text(text = "Assist Chip")},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Filled.Settings,
+                contentDescription = null,
+                modifier = Modifier.size(AssistChipDefaults.IconSize)
+            )
         }
-    }
-    @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
         )
-    }
+}
 
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        MyAndroidTheme {
-            Greeting("Android")
-        }
-    }
