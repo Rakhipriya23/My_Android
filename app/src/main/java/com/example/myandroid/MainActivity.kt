6ebee5e54e52ee7036af6ec10035071704995c2b
Rakhipriya23 <rakhipriyadarshini08@gmail.com>
2024-06-19 20:59:37 +0530

    implented Sliders

--- app/src/main/java/com/example/myandroid/MainActivity.kt
+++ app/src/main/java/com/example/myandroid/MainActivity.kt
@@ -30,10 +30,15 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -57,7 +62,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BadgeM3()
+                    SliderM3()
                 }
             }
 
@@ -67,43 +72,13 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-    //badges
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun BadgeM3() {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            BadgedBox(
-                badge = {
-                    Badge {
-                        Text(text = "99+")
-                    }
-                }
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Email, contentDescription = "Email",
-                    modifier = Modifier
-                        .size(50.dp)
-
-
-                )
-            }
-        }
+@Composable
+fun SliderM3(){
+    var sliderPosition by remember {
+        mutableStateOf(0f)
     }
-    @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-    }
+    Slider(
+        value = sliderPosition ,
+        onValueChange = {sliderPosition = it} )
+}
 
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        MyAndroidTheme {
-            Greeting("Android")
-        }
-    }
