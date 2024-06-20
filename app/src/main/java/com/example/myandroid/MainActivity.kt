38b0811d570378e9c9018d9567019e8dbc3cbc42
Rakhipriya23 <rakhipriyadarshini08@gmail.com>
2024-06-20 22:14:21 +0530

    implented Navigation

--- app/src/main/java/com/example/myandroid/MainActivity.kt
+++ app/src/main/java/com/example/myandroid/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.basicMarquee
@@ -43,12 +44,14 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -61,12 +64,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.myandroid.ui.theme.MyAndroidTheme
 import org.w3c.dom.Text
 
@@ -77,7 +87,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             //Surface(modifier = Modifier.fillMaxSize(),)
 
-            TextFieldSample()
+            AppNavigation()
 
 
             }
@@ -86,24 +96,66 @@ class MainActivity : ComponentActivity() {
 
     @OptIn(ExperimentalFoundationApi::class,ExperimentalMaterial3Api::class)
  @Composable
-fun TextFieldSample(){
+fun AppNavigation(){
+    val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "screen1" ){
+            composable("screen1"){Screen1(navController)}
+            composable("screen2"){Screen2(navController)}
+            composable("screen3/{data}",arguments = listOf(navArgument("date"){type = NavType.StringType})) {backStackEntry ->
+
+            Screen3(navController,backStackEntry.arguments?.getString("data") ?:"")
+                }
+        }
+}
+@Composable
+fun Screen1(navController: NavController){
+    Column(
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxSize()
+    ) {
+        Text(text = "This is Screen1", color = Color.Cyan, fontSize = 50.sp)
+        Button(onClick = {
+            navController.navigate("screen2")
+        }) {
+            Text(text="Goto Screen 2")
+        }
+    }
+}
+@Composable
+fun Screen2(navController: NavController){
     var text by remember {
         mutableStateOf("")
-        
     }
-        Column(
-            modifier = Modifier.padding(30.dp)
-        ) {
-            TextField(value = text, onValueChange = {newtext ->
-                text = newtext
-            }, label = {
-                Text(text = "Enter Anything")
-            }, modifier = Modifier.fillMaxWidth(),
-                placeholder = {
-                    Text(text = "placeholder")
-                }
-                )
-            Spacer(modifier = Modifier.height(15.dp))
-            Text(text = "Hello From User Input:$text")
+    Column(
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxSize()
+    ) {
+        OutlinedTextField(value = text, onValueChange ={
+            text=it
+        }, label = { Text(text = "Enter Something")} )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(text = "This is Screen 2", color = Color.Red, fontSize = 50.sp)
+        Button(onClick = {
+            navController.navigate("screen3/$text")
+        }) {
+            Text(text="Goto Screen 3")
         }
+    }
+}
+@Composable
+fun Screen3(navController: NavController,data:String){
+    Column(
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxSize()
+    ) {
+        Text(text = "This is Screen 2", color = Color.Red, fontSize = 50.sp)
+        Button(onClick = {
+            // navController.navigate("screen2")
+        }) {
+            Text(text="Goto Screen 3")
+        }
+    }
 }
